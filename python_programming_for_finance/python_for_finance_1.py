bc60e577084b3902e472ae5c4c65ebd8a491d1e3
luxiaotong <luxiaotong713@sina.com>
2020-04-03 21:57:17 +0800

    move python_for_finance_1

--- python_programming_for_finance/python_for_finance_1.py
+++ python_programming_for_finance/python_for_finance_1.py
@@ -6,8 +6,8 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2016, 12, 31)
+start = dt.datetime(2020, 4, 1)
+end = dt.datetime(2020, 4, 3)
 
 df = web.DataReader('TSLA', 'yahoo', start, end)
 print(df)
