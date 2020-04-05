6ab7ebf19f4211d8be0e0c9573553bf7d8bcb81f
luxiaotong <luxiaotong713@sina.com>
2020-04-05 11:25:57 +0800

    python_for_finance_4

--- python_programming_for_finance/python_for_finance_4.py
+++ python_programming_for_finance/python_for_finance_4.py
@@ -14,8 +14,9 @@ df = pd.read_csv('./python_programming_for_finance/tsla.csv',
                  parse_dates=True, index_col=0)
 
 df = df.iloc[-100:]
-
 print(df)
+
+# https://nbviewer.jupyter.org/github/matplotlib/mplfinance/blob/master/examples/customization_and_styles.ipynb
 mc = mpf.make_marketcolors(up='r', down='g')
 s = mpf.make_mpf_style(marketcolors=mc)
 mpf.plot(df, mav=(2, 20), type='candle', volume=True, style=s)
