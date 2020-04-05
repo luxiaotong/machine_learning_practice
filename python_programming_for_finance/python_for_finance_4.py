d68ee79a4582415d76aa9dc08356ba78bf95e855
luxiaotong <luxiaotong713@sina.com>
2020-04-05 10:50:04 +0800

    python_for_finance_4

---
+++ python_programming_for_finance/python_for_finance_4.py
@@ -0,0 +1,21 @@
+import datetime as dt
+import pandas as pd
+import pandas_datareader.data as web
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+from matplotlib import dates
+import mplfinance as mpf
+
+
+style.use('ggplot')
+
+df = pd.read_csv('./python_programming_for_finance/tsla.csv',
+                 parse_dates=True, index_col=0)
+
+df = df.iloc[-100:]
+
+print(df)
+mc = mpf.make_marketcolors(up='r', down='g')
+s = mpf.make_mpf_style(marketcolors=mc)
+mpf.plot(df, mav=(2, 20), type='candle', volume=True, style=s)
