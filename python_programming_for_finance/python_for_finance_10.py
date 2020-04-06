32f9a53cbf159a600229c6d1add213b5487e8c91
luxiaotong <luxiaotong713@sina.com>
2020-04-06 09:54:00 +0800

    python_for_finance_10

---
+++ python_programming_for_finance/python_for_finance_10.py
@@ -0,0 +1,11 @@
+
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col > requirement:
+            return 1
+        if col < -requirement:
+            return -1
+
+    return 0
